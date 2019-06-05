bb6956c0699ae652a597bcf78f905de7ead1f1cf
18019330063@163.com <18019330063@163.com>
2019-06-05 17:32:23 +0800

    update

--- src/views/Layout/components/Main/index.js
+++ src/views/Layout/components/Main/index.js
@@ -1,8 +1,9 @@
 import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
-import { actionCreators } from '../../store'
+import { Link } from 'react-router-dom'
 
-import { Layout, Icon } from 'antd'
+import { actionCreators } from '../../store'
+import { Layout, Menu, Icon, Dropdown } from 'antd'
 import './index.less'
 
 const { Header, Content } = Layout
@@ -11,14 +12,33 @@ class MainWrapper extends Component {
   render() {
     const { collapsed, toggleMenuHandler } = this.props
 
+    const dropdownMenu = (
+      <Menu>
+        <Menu.Item>设置</Menu.Item>
+        <Menu.Item>个人资料</Menu.Item>
+        <Menu.Item>
+          <Link to="/login">退出</Link>
+        </Menu.Item>
+      </Menu>
+    )
+
     return (
       <Fragment>
         <Header className="main-header">
-          <Icon
-            className="trigger"
-            type={collapsed ? 'menu-unfold' : 'menu-fold'}
-            onClick={toggleMenuHandler}
-          />
+          <div className="main-header__left">
+            <Icon
+              className="trigger"
+              type={collapsed ? 'menu-unfold' : 'menu-fold'}
+              onClick={toggleMenuHandler}
+            />
+          </div>
+          <div className="main-header__right">
+            <Icon type="message" className="main-header__icon" />
+            &nbsp;&nbsp;&nbsp;
+            <Dropdown overlay={dropdownMenu} placement="bottomRight" trigger={['click']}>
+              <Icon type="user" className="main-header__icon" />
+            </Dropdown>
+          </div>
         </Header>
         <Content>
           Content
